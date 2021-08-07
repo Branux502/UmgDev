73496435353fce557856550a7c2cbf3ee6d8bd91
brayan <brayanmiranda333@gmail.com>
2021-08-06 20:26:47 -0600

    Second commit Consulta dos minuto 3

---
+++ DXWebApplication/App_Code/Dal/DaoMantenimientos/ClsDaoProducto.cs
@@ -0,0 +1,30 @@
+﻿using DXWebApplication.App_Code.Utilidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DXWebApplication.App_Code.Dal.DaoMantenimientos
+{
+    public class ClsDaoProducto : ClsDataLayer
+    {
+        ClsConexion objSql = new ClsConexion();
+        ClsErrorHandler log = new ClsErrorHandler();
+        string strSql = "";
+        //Estructura de un metodo para obtener informacion de la BD
+        public bool GetProductoAll()
+        {
+            try
+            {
+                strSql = "SELECT ID_PRODUCTO, ID_TIPO_PRODUCTO, DESCRIPCION, PRECIO, EXISTENCIA, ESTADO FROM POS.PRODUCTO  ";
+                DsReturn = objSql.EjectuaSQL(strSql, "Producto");
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.ToString(), ex.StackTrace);
+                return false;
+            }
+            return true;
+        }
+    }
+}
